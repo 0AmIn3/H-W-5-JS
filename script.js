e01c1a24b7945f02fb29f1f43f92b8018abba868
0AmIn3 <aminfedya2888@gmail.com>
2022-06-16 10:49:53 +0300

    H-W №5 JS

---
+++ script.js
@@ -0,0 +1,16 @@
+let arr = ['alish', 'amir' , 'amin' , 'zarina' , 'shoxrux' , 'kumush' , 'amina' , 'xoji' , 'shox' , 'ula' , 'artyom']
+let name = prompt('what is ur name?').toLowerCase().trim()
+
+if( name === arr[0]){alert(`Welcome ${arr[0].slice(0,1).toUpperCase() + arr[0].slice(1)}`)}
+else if(name === arr[1]){alert(`Welcome ${arr[1].slice(0,1).toUpperCase() + arr[1].slice(1)} `)}
+else if(name === arr[2]){alert(`Welcome ${arr[2].slice(0,1).toUpperCase() + arr[2].slice(1)}`)}
+else if(name === arr[3]){alert(`Welcome ${arr[3].slice(0,1).toUpperCase() + arr[3].slice(1)}`)}
+else if(name === arr[4]){alert(`Welcome ${arr[4].slice(0,1).toUpperCase() + arr[4].slice(1)}`)}
+else if(name === arr[5]){alert(`Welcome ${arr[5].slice(0,1).toUpperCase() + arr[5].slice(1)}`)}
+else if(name === arr[6]){alert(`Welcome ${arr[6].slice(0,1).toUpperCase() + arr[6].slice(1)}`)}
+else if(name === arr[7]){alert(`Welcome ${arr[7].slice(0,1).toUpperCase() + arr[7].slice(1)}`)}
+else if(name === arr[8]){alert(`Welcome ${arr[8].slice(0,1).toUpperCase() + arr[8].slice(1)}`)}
+else if(name === arr[9]){alert(`Welcome ${arr[9].slice(0,1).toUpperCase() + arr[9].slice(1)}`)}
+else if(name === arr[10]){alert(`Welcome ${arr[10].slice(0,1).toUpperCase() + arr[10].slice(1)}`)}
+else{alert(`get out`)}
+//
